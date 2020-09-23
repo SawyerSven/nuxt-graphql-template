26b28503129f451840b87d7a85426fbae37dce84
sunsiyuan <siyuan.sun1@baozun.com>
2020-09-23 14:58:15 +0800

    feat: 修改env API_URL指向地址

--- src/config/env.js
+++ src/config/env.js
@@ -2,13 +2,13 @@ let API_URL = ""
 
 switch (process.env.API_ENV) {
   case "sandbox":
-    API_URL = process.server ? "http://brembo-backend-uat.cloud.bz/graphql" : '/graphql'
+    API_URL = process.server || process.env.NODE_ENV === "production" ? "http://brembo-backend-uat.cloud.bz/graphql" : '/graphql'
     break
   case "uat":
-    API_URL = process.server ? "http://brembo-backend-uat.baozun.com/graphql" : '/graphql'
+    API_URL = process.server || process.env.NODE_ENV === "production" ? "http://brembo-backend-uat.baozun.com/graphql" : '/graphql'
     break
   default:
-    API_URL = process.server ? "https://api.liaoliaojun.com:3000/graphql" : "/graphql"
+    API_URL = process.server || process.env.NODE_ENV === "production" ? "https://api.liaoliaojun.com:3000/graphql" : "/graphql"
     break
 }
 
